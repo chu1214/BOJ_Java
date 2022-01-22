44cd1d05104fa60f3e02c59fa0695df6a08924a4
황승주 <las1260@naver.com>
2022-01-22 23:04:29 +0900

    220122

--- src/boj/str/BOJ_str_B2_1152.java
+++ src/boj/str/BOJ_str_B2_1152.java
@@ -1,10 +1,18 @@
 package boj.str;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BOJ_str_B2_1152 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		br.close();
+		
+		System.out.print(st.countTokens());
+		}
 }
