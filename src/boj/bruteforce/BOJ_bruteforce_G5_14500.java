7a453dc828d865baebb0498c36cb310f878c3b4b
황승주 <las1260@naver.com>
2022-02-10 16:39:44 +0900

    220210

--- src/boj/bruteforce/BOJ_bruteforce_G5_14500.java
+++ src/boj/bruteforce/BOJ_bruteforce_G5_14500.java
@@ -1,47 +1,54 @@
 package boj.bruteforce;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class BOJ_bruteforce_G5_14500 {
 
 	static int N, M;
-	static int score[][];
+	static int[][] score;
+	static boolean[][] visited;
+	static int[][] deltas = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 	static int maxScore = 0;
-	static int currentScore = 0;
+
+	public static void makeT(int r, int c, int sum) {
+		if(r >= 0 && r < N - 1 && c > 0 && c < M - 1) {
+			sum = score[r][c] + score[r][c - 1] + score[r][c + 1] + score[r + 1][c];
+			if(sum > maxScore) maxScore = sum;
+		}
+		if(r > 0 && r < N - 1 && c > 0 && c <= M - 1) {
+			sum = score[r][c] + score[r - 1][c] + score[r + 1][c] + score[r][c - 1];
+			if(sum > maxScore) maxScore = sum;
+		}
+		if(r > 0 && r <= N - 1 && c > 0 && c < M - 1) {
+			sum = score[r][c] + score[r][c - 1] + score[r][c + 1] + score[r - 1][c];
+			if(sum > maxScore) maxScore = sum;
+		}
+		if(r > 0 && r < N - 1 && c >= 0 && c < M - 1) {
+			sum = score[r][c] + score[r - 1][c] + score[r + 1][c] + score[r][c + 1];
+			if(sum > maxScore) maxScore = sum;
+		}
+	}
 	
-	public static void make(int cnt, int r, int c) {
-		currentScore += score[r][c];
-		int temp = score[r][c];
-		score[r][c] = 0;
-		
+	public static void dfs(int r, int c, int sum, int cnt) {
 		if(cnt == 4) {
-			if(maxScore < currentScore) {
-				maxScore = currentScore;
-				for(int i = 0; i < N; i++) {
-					for(int j = 0; j < M; j++) {
-						System.out.print(score[i][j] + " ");
-					}
-					System.out.println();
-				}
-			}
-			currentScore = 0;
-			score[r][c] = temp;
-			currentScore -= score[r][c];
+			if(sum > maxScore) maxScore = sum;
 			return;
 		}
 		
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < M; j++) {
-				if(Math.abs((r + c) - (i + j)) == 1 && score[i][j] != 0) {
-					make(cnt + 1, i, j);
-				}
+		visited[r][c] = true;		
+		for(int d = 0; d < 4; d++) {
+			int nr = r + deltas[d][0];
+			int nc = c + deltas[d][1];
+			if(isIn(nr, nc) && !visited[nr][nc]) {
+				dfs(nr, nc, sum + score[nr][nc], cnt + 1);
 			}
 		}
-		score[r][c] = temp;
-		currentScore -= score[r][c];
+		visited[r][c] = false;
+	}
+	
+	public static boolean isIn(int nr, int nc) {
+		return nr >= 0 && nr < N && nc >= 0 && nc < M;
 	}
 	
 	public static void main(String[] args) throws IOException {
@@ -52,6 +59,7 @@ public class BOJ_bruteforce_G5_14500 {
 		M = Integer.parseInt(st.nextToken());
 		
 		score = new int[N][M];
+		visited = new boolean[N][M];
 		
 		for(int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -62,7 +70,8 @@ public class BOJ_bruteforce_G5_14500 {
 		
 		for(int i = 0; i < N; i++) {
 			for(int j = 0; j < M; j++) {
-				make(0, i, j);
+				dfs(i, j, 0, 0);
+				makeT(i, j, 0);
 			}
 		}
 		
