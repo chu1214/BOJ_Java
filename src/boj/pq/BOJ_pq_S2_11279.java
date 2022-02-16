545d83357105476cb7db9f5f590efb26bbf2738a
황승주 <las1260@naver.com>
2022-02-16 15:40:13 +0900

    220216

---
+++ src/boj/pq/BOJ_pq_S2_11279.java
@@ -0,0 +1,35 @@
+package boj.pq;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class BOJ_pq_S2_11279 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+		
+		for(int i = 0; i < N; i++) {
+			int C = Integer.parseInt(br.readLine());
+			if(C == 0) {
+				if(!pq.isEmpty()) {
+					sb.append(pq.poll() + "\n");
+				} else {
+					sb.append("0\n");
+				}
+			} else {
+				pq.offer(C);
+			}
+		}
+		sb.setLength(sb.length() - 1);
+		System.out.println(sb.toString());
+
+	}
+
+}
