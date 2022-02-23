e523ae4ff36f2c6470f602abc3eb2629add65b79
황승주 <las1260@naver.com>
2022-02-23 20:56:03 +0900

    220223

---
+++ src/boj/math/BOJ_math_B2_2292.java
@@ -0,0 +1,25 @@
+package boj.math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_math_B2_2292 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		
+		int sum = 1;
+		int i = 0;
+		
+		while(true) {
+			sum += 6 * i; 
+			i++;
+			System.out.println(sum + " " + i);
+			if(N < sum + 1) break;
+		}
+		System.out.println(i);
+	}
+}
