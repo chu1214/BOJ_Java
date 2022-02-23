d33b671320acd5b0190b6a0dae00fab759060e36
황승주 <las1260@naver.com>
2022-02-23 20:10:53 +0900

    220223

---
+++ src/boj/dp/BOJ_dp_S1_1932.java
@@ -0,0 +1,44 @@
+package boj.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_dp_S1_1932 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int N = Integer.parseInt(br.readLine());
+		
+		int[][] triangle = new int[N][N];
+		int[][] dp = new int[N][N];
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j <= i; j++) {
+				triangle[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		dp[0][0] = triangle[0][0];
+				
+		for(int i = 1; i < N; i++) {
+			for(int j = 0; j <= i; j++) {
+				if(j == 0) dp[i][j] = dp[i - 1][j] + triangle[i][j];
+				else if(j == i) dp[i][j] = dp[i - 1][j - 1] + triangle[i][j];
+				else dp[i][j] = Math.max(dp[i - 1][j - 1] + triangle[i][j], dp[i - 1][j] + triangle[i][j]);
+			}
+		}
+		
+		int max = 0;
+		
+		for(int num : dp[N-1]) {
+			max = Math.max(max, num);
+		}
+		
+		System.out.println(max);
+	}
+}
