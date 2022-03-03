7ddea7ee032031945a345919d71da04a73343b1f
황승주 <las1260@naver.com>
2022-03-03 21:56:45 +0900

    220303

--- src/boj/dfs/BOJ_dfs_G4_1967.java
+++ src/boj/dfs/BOJ_dfs_G4_1967.java
@@ -19,8 +19,22 @@ public class BOJ_dfs_G4_1967 {
 		
 	}
 	
-	static int N;
-	static ArrayList<Integer>[] list;
+	static int N, ans = 0;
+	static ArrayList<Node>[] list;
+	static boolean[] visited;
+	
+	public static void dfs(int cur, int sum) {
+		boolean isEnd = true;
+		visited[cur] = true;
+		for(int i = 0; i < list[cur].size(); i++) {
+			int next = list[cur].get(i).next;
+			int cost = list[cur].get(i).cost;
+			if(visited[next]) continue;
+			isEnd = false;
+			dfs(next, sum + cost);
+		}
+		if(isEnd) ans = Math.max(ans, sum);
+	}
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,14 +42,26 @@ public class BOJ_dfs_G4_1967 {
 
 		N = Integer.parseInt(br.readLine());
 		
-		list
+		list = new ArrayList[N+1];
 		
-		for(int i = 0; i < N; i++) {
+		for(int i = 1; i <= N; i++) {
+			list[i] = new ArrayList<>();
+		}
+		
+		for(int i = 0; i < N - 1; i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
+			list[a].add(new Node(b, c));
+			list[b].add(new Node(a, c));
 		}
+		
+		for(int i = 1; i <= N; i++) {
+			visited = new boolean[N+1];
+			dfs(i, 0);
+		}
+		
+		System.out.println(ans);
 	}
-
 }
