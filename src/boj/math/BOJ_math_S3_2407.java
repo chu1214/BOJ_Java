5d3b6dda3ad81498ff632dd3d27ea431c7c919bf
Seungju Hwang <90178949+chu1214@users.noreply.github.com>
2022-03-05 18:53:57 +0900

    Create BOJ_math_S3_2407.java

---
+++ src/boj/math/BOJ_math_S3_2407.java
@@ -0,0 +1,36 @@
+package boj.math
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.util.StringTokenizer;
+
+public class BOJ_Math_S3_2407 {
+
+	public static BigDecimal combination(int N, int M) {
+		BigDecimal ans = new BigDecimal(1);
+		
+		for(int i = N; i > N-M; i--) {
+			ans = ans.multiply(new BigDecimal(i));
+		}
+		
+		for(int i = M; i > 0; i--) {
+			ans = ans.divide(new BigDecimal(i));
+		}
+		
+		return ans;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		System.out.println(combination(N,M));
+
+	}
+
+}
