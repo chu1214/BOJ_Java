d3617a321a1cd0c7fd70e83e7c45e5d7a6f7f81d
황승주 <las1260@naver.com>
2022-03-08 18:19:38 +0900

    220308

---
+++ src/boj/math/BOJ_math_B5_8370.java
@@ -0,0 +1,21 @@
+package boj.math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_math_B5_8370 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n1 = Integer.parseInt(st.nextToken());
+		int k1 = Integer.parseInt(st.nextToken());
+		int n2 = Integer.parseInt(st.nextToken());
+		int k2 = Integer.parseInt(st.nextToken());
+
+		System.out.println(n1*k1 + n2*k2);
+	}
+}
