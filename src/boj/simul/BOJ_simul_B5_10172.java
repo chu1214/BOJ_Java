ae548dc2dc345e6f90b9a48246d6349bd24bf564
황승주 <las1260@naver.com>
2022-03-21 10:04:27 +0900

    220320

---
+++ src/boj/simul/BOJ_simul_B5_10172.java
@@ -0,0 +1,16 @@
+package boj.simul;
+
+public class BOJ_simul_B5_10172 {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("|\\_/|\n");
+		sb.append("|q p|   /}\n");
+		sb.append("( 0 )\"\"\"\\\n");
+		sb.append("|\"^\"`    |\n");
+		sb.append("||_/=\\\\__|\n");
+
+		System.out.println(sb.toString());
+	}
+}
