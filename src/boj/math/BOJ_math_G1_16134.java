11643660b7c3aeff5df65e912c8f68b5872dfd8e
황승주 <las1260@naver.com>
2022-04-13 13:25:59 +0900

    220413

--- src/boj/math/BOJ_math_G1_16134.java
+++ src/boj/math/BOJ_math_G1_16134.java
@@ -5,22 +5,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BOJ_math_G1_11401 {
+public class BOJ_math_G1_16134 {
 
-	static int N, K;
+	static int N, R;
 	static long[] fac;
 	static final long MOD = 1000000007;
 
-	public static long multiply(int n, int k, long b) {
+	public static long multiply(int n, int r, long b) {
 		if (b == 1) {
-			return fac[k] * fac[n - k] % MOD;
+			return fac[r] * fac[n - r] % MOD;
 		}
 
-		long half = multiply(n, k, b / 2);
+		long half = multiply(n, r, b / 2);
 		long res = half * half % MOD;
 
 		if (b % 2 == 1) {
-			res = (res * multiply(n, k, 1)) % MOD;
+			res = res * multiply(n, r, 1) % MOD;
 		}
 
 		return res;
@@ -31,17 +31,18 @@ public class BOJ_math_G1_11401 {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
+		R = Integer.parseInt(st.nextToken());
 
 		fac = new long[N + 1];
 		fac[0] = 1;
 
 		for (int i = 1; i <= N; i++) {
-			fac[i] = (i * fac[i - 1]) % MOD;
+			fac[i] = i * fac[i - 1] % MOD;
 		}
 
-		long ans = fac[N] * multiply(N, K, MOD - 2) % MOD;
+		long ans = fac[N] * multiply(N, R, MOD - 2) % MOD;
 
 		System.out.println(ans);
 	}
+
 }
